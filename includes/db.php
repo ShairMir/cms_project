80dff1bf1db063894483a4cc0fe478769c93ec19
Shair Mir <s.mir89@hotmail.com>
2015-11-22 13:17:17 +0100

    Refactoring + small changes

--- includes/db.php
+++ includes/db.php
@@ -16,12 +16,3 @@ if(!$connection) {
 }
 
 ?>
-
-
-                <!-- $query = "SELECT * FROM categories";
-                $select_all_categories_query = mysqli_query($connection, $query);
-
-                while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
-                    $cat_title = $row['cat_title'];
-                    echo "<li><a href='#'>{$cat_title}</a></li>";
-                } -->
