4b143b7ac5f34510de884e67e6db17eeb489985a
Shair Mir <s.mir89@hotmail.com>
2015-11-22 19:55:14 +0100

    New functionalities
    - able to make posts
    - able to deleting posts

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,30 +1,6 @@
 <?php 
 
-if (isset($_POST['create_post'])) {
-	
-	$post_title = $_POST['title'];
-	$post_author = $_POST['author'];
-	$post_category_id = $_POST['post_category_id'];
-	$post_status = $_POST['post_status'];
 
-	$post_image = $_FILES['image']['name'];
-	$post_image_temp = $_FILES['image']['tmp_name'];
-
-	$post_tags = $_POST['post_tags'];
-	$post_content = $_POST['post_content'];
-	$post_date = date('d-m-y');
-	$post_comment_count = 5;
-
-	move_uploaded_file($post_image_temp, "../images/$post_image");
-
-	$query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) ";
-	$query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_comment_count}','{$post_status}' ) ";
-
-	$create_post_query = mysqli_query($connection, $query);
-
-	confirm($create_post_query);
-
-}
 
 ?>
 
@@ -67,7 +43,7 @@ if (isset($_POST['create_post'])) {
 	</div>
 
 	<div class="form-group">
-		<input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+		<input type="submit" class="btn btn-primary" name="edit_post" value="Edit Post">
 	</div>
 
 
