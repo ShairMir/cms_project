84878aa22397336da8f6d1234e8be455ccd2a277
Shair Mir <s.mir89@hotmail.com>
2015-11-23 13:47:11 +0100

    Editing posts changes

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,11 +1,12 @@
 <?php 
 
-if (isset($_GET['$p_id'])) {
-	$the_post_id = $_GET['$p_id'];
-    echo $the_post_id;
+if (isset($_GET['p_id'])) {
+	$the_post_id = $_GET['p_id'];
+} else {
+	die("GET p_id FAILED" . mysqli_error($connection));
 }
 
-$query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts WHERE post_id = $the_post_id";
 $select_posts_by_id = mysqli_query($connection, $query);
 
 while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
@@ -19,18 +20,60 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_tags = $row['post_tags'];
     $post_comment_count = $row['post_comment_count'];
     $post_date = $row['post_date'];
+
+    $post_id = mysqli_real_escape_string($connection, $post_id);
+    $post_author = mysqli_real_escape_string($connection, $post_author);
+    $post_title = mysqli_real_escape_string($connection, $post_title);
+    $post_category_id = mysqli_real_escape_string($connection, $post_category_id);
+    $post_status = mysqli_real_escape_string($connection, $post_status);
+    $post_image = mysqli_real_escape_string($connection, $post_image);
+    $post_content = mysqli_real_escape_string($connection, $post_content);
+    $post_tags = mysqli_real_escape_string($connection, $post_tags);
+    $post_comment_count = mysqli_real_escape_string($connection, $post_comment_count);
+    $post_date = mysqli_real_escape_string($connection, $post_date);
+
 }
+
+if (isset($_POST['update_post'])) {
+
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+    move_uploaded_file($post_image_temp, "./images/$post_image");
+
+    $query = "UPDATE posts SET ";
+    $query .= "post_title = '{$post_title}', ";
+    $query .= "post_category_id = '{$post_category_id}', ";
+    $query .= "post_date = now(), ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags = '{$post_tags}', ";
+    $query .= "post_content = '{$post_content}', ";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id}";
+	
+	$update_post = mysqli_query($connection, $query);
+
+	confirmQuery($update_post);
+}
+
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
 	
 	<div class="form-group">
 		<label for="title">Edit Title</label>
-		<input value="<?php echo $post_title;?>" type="text" class="form-control" name="title">
+		<input value="<?php echo $post_title;?>" type="text" class="form-control" name="post_title">
 	</div>
 	
 	<div class="form-group">
-		<select name="" id="">
+		<select name="post_category" id="">
 			
 			<?php 
 
@@ -43,7 +86,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 				$cat_id = $row['cat_id'];
 				$cat_title = $row['cat_title'];
 
-				echo "<option value=''>{$cat_title}</option>";
+				echo "<option value='$cat_id'>{$cat_title}</option>";
 
 			}
 
@@ -56,7 +99,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 	
 	<div class="form-group">
 		<label for="title">Post Author</label>
-		<input value="<?php echo $post_author;?>" type="text" class="form-control" name="author">
+		<input value="<?php echo $post_author;?>" type="text" class="form-control" name="post_author">
 	</div>
 
 	<div class="form-group">
@@ -79,7 +122,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 	</div>
 
 	<div class="form-group">
-		<input type="submit" class="btn btn-primary" name="edit_post" value="Update Post">
+		<input type="submit" class="btn btn-primary" name="update_post" value="Update Post">
 	</div>
 
 
