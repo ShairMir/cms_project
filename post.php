f4c5044631a472f20ddac80e2190606e6a5b8a10
Shair Mir <s.mir89@hotmail.com>
2015-11-24 12:08:41 +0100

    New Fields in comment form

--- post.php
+++ post.php
@@ -57,15 +57,38 @@
 
             
                 <!-- Blog Comments -->
+                
+
+                <?php 
+
+                if (isset($_POST['create_comment'])) {
+                    echo $_POST['comment_author'];
+                }
+
+                ?>
 
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <form action="" method="post" role="form">
+
                         <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                            <label for="Author">Author</label>
+                            <input class="form-control" type="text" name="comment_author">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+
+                        <div class="form-group">
+                            <label for="Email">Email</label>
+                            <input class="form-control" type="email" name="comment_email">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="Comment">Your Comment</label>
+                            <textarea class="form-control" rows="3" name="comment_content"></textarea>
+                        </div>
+
+
+                        <button type="submit" class="btn btn-primary" name="create_comment">Submit</button>
                     </form>
                 </div>
 
