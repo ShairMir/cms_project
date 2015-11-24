8ddf13fa4f00a93cc78d8425db642eb970613f48
Shair Mir <s.mir89@hotmail.com>
2015-11-24 14:15:29 +0100

    Relating Comments to posts

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -50,7 +50,24 @@
 
                 echo "<td>{$comment_email}</td>";
                 echo "<td>{$comment_status}</td>";
-                echo "<td>Some Title</td>";
+
+
+                $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+                $select_post_id_query = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_assoc($select_post_id_query)) {
+                    $post_id = $row['post_id'];
+                    $post_title = $row['post_title'];
+
+                    echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
+                }
+
+                
+
+
+
+
+
                 echo "<td>{$comment_date}</td>";
                 echo "<td><a href=''>Approve</a></td>";
                 echo "<td><a href=''>Unapprove</a></td>";
