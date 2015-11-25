367652aa3f5896ea31a3f98afcad91fde696362d
Shair Mir <s.mir89@hotmail.com>
2015-11-25 15:19:51 +0100

    Displaying user profile data

--- admin/profile.php
+++ admin/profile.php
@@ -3,7 +3,23 @@
 <?php 
 
 if (isset($_SESSION['username'])) {
-    $_SESSION['username'];
+    $username = $_SESSION['username'];
+
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
+
+    $select_user_profile_query = mysqli_query($connection, $query);
+
+    while ($row = mysqli_fetch_assoc($select_user_profile_query)) {
+
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password = $row['user_password'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_image = $row['user_image'];
+        $user_role = $row['user_role'];
+    }
 
 }
 
@@ -78,7 +94,7 @@ if (isset($_SESSION['username'])) {
                             </div>  
 
                             <div class="form-group">
-                                <input type="submit" class="btn btn-primary" name="update_user" value="Update User">
+                                <input type="submit" class="btn btn-primary" name="update_user" value="Update Profile">
                             </div>
                         </form>
 
