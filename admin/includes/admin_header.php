63b6cdb9385c17e1ad57428e11008660730459aa
Shair Mir <s.mir89@hotmail.com>
2015-11-25 18:15:03 +0100

    Added Google Chart

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -40,6 +40,7 @@ if (!isset($_SESSION['user_role'])) {
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
 </head>
 
 <body>
