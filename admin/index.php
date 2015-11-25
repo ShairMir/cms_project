18c454695fff530e52c76c5c2290cee3da970551
Shair Mir <s.mir89@hotmail.com>
2015-11-25 18:23:57 +0100

    Cleaning up chart

--- admin/index.php
+++ admin/index.php
@@ -156,21 +156,26 @@
 
             <div class="row">
                 <script type="text/javascript">
-                  google.load("visualization", "1.1", {packages:["bar"]});
-                  google.setOnLoadCallback(drawChart);
-                  function drawChart() {
-                    var data = google.visualization.arrayToDataTable([
-                      ['Year', 'Sales', 'Expenses', 'Profit'],
-                      ['2014', 1000, 400, 200],
-                      ['2015', 1170, 460, 250],
-                      ['2016', 660, 1120, 300],
-                      ['2017', 1030, 540, 350]
-                    ]);
+                    google.load("visualization", "1.1", {packages:["bar"]});
+                    google.setOnLoadCallback(drawChart);
+                    function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                        ['Data', 'Count'],
 
-                    var options = {
-                      chart: {
-                        title: 'Company Performance',
-                        subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                        <?php 
+
+                        
+
+                        ?>
+
+                        ['Posts', 1000],
+                      
+                        ]);
+
+                        var options = {
+                        chart: {
+                        title: '',
+                        subtitle: '',
                       }
                     };
 
@@ -179,7 +184,7 @@
                     chart.draw(data, options);
                   }
                 </script>
-                <div id="columnchart_material" style="width: 900px; height: 500px;"></div>
+                <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
             </div>
 
 
