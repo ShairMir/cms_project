a544eea374de91fc4fe19abe60cfcbdad5a7919d
Shair Mir <s.mir89@hotmail.com>
2015-11-25 12:21:57 +0100

    Login
    - create login.php with functionality
    - create login form on homepage

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -17,6 +17,23 @@
                     <!-- /.input-group -->
                 </div>
 
+                <!-- Login -->
+                <div class="well">
+                    <h4>Login</h4>
+                    <form action="includes/login.php" method="post">
+                        <div class="form-group">
+                            <input name="username" type="text" class="form-control" placeholder="Username">
+                        </div>
+                        <div class="input-group">
+                            <input name="password" type="password" class="form-control" placeholder="Password">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" name="login" type="submit">Submit</button>
+                            </span>
+                        </div>
+                    </form> <!-- login form -->
+                    <!-- /.form-group -->
+                </div>
+
                 <!-- Blog Categories Well -->
                 <div class="well">
 
