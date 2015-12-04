39697748876b805f5ae87a0e6a6ad19af7485d00
Shair Mir <s.mir89@hotmail.com>
2015-12-04 12:34:13 +0100

    Adding Bulk Options to Posts
    Part 1
    - added form
    - added checkboxes

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,70 +1,99 @@
-<table class="table table-bordered table-hover">
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Author</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Status</th>
-            <th>Image</th>
-            <th>Tags</th>
-            <th>Comments</th>
-            <th>Date</th>
-            <th>Edit</th>
-            <th>Delete</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-        <?php 
+<form action="" method="post">
+    <table class="table table-bordered table-hover">
 
-        if (isset($connection)) {
+        <div id="bulkOptionContainer" class="col-xs-4">
+            <select class="form-control" name="" id="">
+                <option value="">Select Options</option>
+                <option value="">Publish</option>
+                <option value="">Draft</option>
+                <option value="">Delete</option>
+            </select>
+            <br>
+        </div>
+
+        <div class="col-xs-4">
+                
+            <input type="submit" name="submit" class="btn btn-success" value="Apply">
+            <a class="btn btn-primary" href="add_post.php">Add New</a>
+
+        </div>
+
+
+        <thead>
+            <tr>
+                <th><input id="selectAllBoxes" type="checkbox"></th>
+                <th>Id</th>
+                <th>Author</th>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Status</th>
+                <th>Image</th>
+                <th>Tags</th>
+                <th>Comments</th>
+                <th>Date</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
             
-            $query = "SELECT * FROM posts";
-            $select_all_posts = mysqli_query($connection, $query);
+            <?php 
 
-            while ($row = mysqli_fetch_assoc($select_all_posts)) {
-                $post_id = $row['post_id'];
-                $post_author = $row['post_author'];
-                $post_title = $row['post_title'];
-                $post_category_id = $row['post_category_id'];
-                $post_status = $row['post_status'];
-                $post_image = $row['post_image'];
-                $post_tags = $row['post_tags'];
-                $post_comment_count = $row['post_comment_count'];
-                $post_date = $row['post_date'];
+            if (isset($connection)) {
+                
+                $query = "SELECT * FROM posts";
+                $select_all_posts = mysqli_query($connection, $query);
 
-                echo "<tr>";
-                echo "<td>{$post_id}</td>";
-                echo "<td>{$post_author}</td>";
-                echo "<td>{$post_title}</td>";
+                while ($row = mysqli_fetch_assoc($select_all_posts)) {
+                    $post_id = $row['post_id'];
+                    $post_author = $row['post_author'];
+                    $post_title = $row['post_title'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_status = $row['post_status'];
+                    $post_image = $row['post_image'];
+                    $post_tags = $row['post_tags'];
+                    $post_comment_count = $row['post_comment_count'];
+                    $post_date = $row['post_date'];
 
-                // Query for showing the post categories dynamically
-                $query = "SELECT * FROM categories WHERE cat_id = $post_category_id ";
-                $select_categories_id = mysqli_query($connection, $query);
+                    echo "<tr>";
 
-                while ($row = mysqli_fetch_assoc($select_categories_id)) {
-                    $cat_id = $row['cat_id'];
-                    $cat_title = $row['cat_title'];
+                    ?>
 
-                    echo "<td>{$cat_title}</td>";
-                }
+                    <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id; ?>'></td>
+                    
+                    <?php
 
-                echo "<td>{$post_status}</td>";
-                echo "<td><img src='../images/{$post_image}' width='100' alt = 'image'></td>";
-                echo "<td>{$post_tags}</td>";
-                echo "<td>{$post_comment_count}</td>";
-                echo "<td>{$post_date}</td>";
-                echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-                echo "<td><a href='posts.php?delete=$post_id'>Delete</a></td>";
-                echo "</tr>";
-            }        
-        } 
+                    echo "<td>{$post_id}</td>";
+                    echo "<td>{$post_author}</td>";
+                    echo "<td>{$post_title}</td>";
 
-        ?>
+                    // Query for showing the post categories dynamically
+                    $query = "SELECT * FROM categories WHERE cat_id = $post_category_id ";
+                    $select_categories_id = mysqli_query($connection, $query);
 
-    </tbody>
- </table>
+                    while ($row = mysqli_fetch_assoc($select_categories_id)) {
+                        $cat_id = $row['cat_id'];
+                        $cat_title = $row['cat_title'];
+
+                        echo "<td>{$cat_title}</td>";
+                    }
+
+                    echo "<td>{$post_status}</td>";
+                    echo "<td><img src='../images/{$post_image}' width='100' alt = 'image'></td>";
+                    echo "<td>{$post_tags}</td>";
+                    echo "<td>{$post_comment_count}</td>";
+                    echo "<td>{$post_date}</td>";
+                    echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
+                    echo "<td><a href='posts.php?delete=$post_id'>Delete</a></td>";
+                    echo "</tr>";
+                }        
+            } 
+
+            ?>
+
+        </tbody>
+     </table>
+ </form>
 
 
  <?php 
