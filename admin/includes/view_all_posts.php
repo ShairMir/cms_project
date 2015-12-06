8fe7e9d4ebafe67affa040979787cc64b1f10a89
Shair Mir <s.mir89@hotmail.com>
2015-12-06 17:02:35 +0100

    Post View Count Added

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -90,6 +90,7 @@ if (isset($_POST['checkBoxArray'])) {
                 <th>Tags</th>
                 <th>Comments</th>
                 <th>Date</th>
+                <th>View Count</th>
                 <th>View</th>
                 <th>Edit</th>
                 <th>Delete</th>
@@ -114,6 +115,7 @@ if (isset($_POST['checkBoxArray'])) {
                     $post_tags = $row['post_tags'];
                     $post_comment_count = $row['post_comment_count'];
                     $post_date = $row['post_date'];
+                    $post_views_count = $row['post_views_count'];
 
                     echo "<tr>";
 
@@ -144,6 +146,7 @@ if (isset($_POST['checkBoxArray'])) {
                     echo "<td>{$post_tags}</td>";
                     echo "<td>{$post_comment_count}</td>";
                     echo "<td>{$post_date}</td>";
+                    echo "<td>{$post_views_count}</td>";
                     echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                     echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this post?')\" href='posts.php?delete=$post_id'>Delete</a></td>";
--- post.php
+++ post.php
@@ -17,42 +17,55 @@
 
                 if (isset($_GET['p_id'])) {
                     $the_post_id = $_GET['p_id'];
+
+                    $query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = $the_post_id ";
+                    $send_query = mysqli_query($connection, $query);
+
+                    if(!$send_query) {
+                        die("Query Failed");
+                    }
+
+                    $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+                    $select_all_posts_query = mysqli_query($connection, $query);
+
+                    while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_image = $row['post_image'];
+                        $post_content = $row['post_content'];
+                        $post_tags = $row['post_tags'];
+                    
+                    ?>
+
+                        <h1 class="page-header">
+                            Page Heading
+                            <small>Secondary Text</small>
+                        </h1>
+
+                        <!-- First Blog Post -->
+                        <h2>
+                            <a href="#"><?php echo $post_title ?></a>
+                        </h2>
+                        <p class="lead">
+                            by <a href="index.php"><?php echo $post_author ?></a>
+                        </p>
+                        <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+                        <hr>
+                        <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                        <hr>
+                        <p><?php echo $post_content ?></p>
+
+                        <hr>
+
+              <?php }
+
+                } else {
+                    header("Location: index.php");
                 }
 
-                $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
-                $select_all_posts_query = mysqli_query($connection, $query);
 
-                while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                    $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
-                    $post_date = $row['post_date'];
-                    $post_image = $row['post_image'];
-                    $post_content = $row['post_content'];
-                    $post_tags = $row['post_tags'];
-                
                 ?>
-
-                    <h1 class="page-header">
-                        Page Heading
-                        <small>Secondary Text</small>
-                    </h1>
-
-                    <!-- First Blog Post -->
-                    <h2>
-                        <a href="#"><?php echo $post_title ?></a>
-                    </h2>
-                    <p class="lead">
-                        by <a href="index.php"><?php echo $post_author ?></a>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
-                    <hr>
-                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                    <hr>
-                    <p><?php echo $post_content ?></p>
-
-                    <hr>
-
-                <?php } ?>
 
             
                 <!-- Blog Comments -->
@@ -158,13 +171,6 @@
 
                 ?>
                
-
-
-              
-
-
-                
-
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
