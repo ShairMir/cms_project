9748b97a495a51765f1ec0b29cb986cdb3653a0e
Shair Mir <s.mir89@hotmail.com>
2015-12-06 13:28:08 +0100

    Security changes and more
    - Encrypt passwords when editing
    - Show current value of user_role after editing
    - Add notification when update is successful and give a link to view
    all users

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -34,18 +34,32 @@ if (isset($_POST['edit_user'])) {
 
 	// move_uploaded_file($post_image_temp, "./images/$post_image");
 
+	// Select the default Salt value from the database, used for password encryption
+	$query = "SELECT randSalt FROM users";
+	$select_randsalt_query = mysqli_query($connection, $query);
+	if (!$select_randsalt_query) {
+		die("Query Failed" . mysqli_error($connection));
+	}
+
+	$row = mysqli_fetch_array($select_randsalt_query);
+	$salt = $row['randSalt'];
+	$hashed_password = crypt($user_password, $salt);
+
+	// Update DB
 	$query = "UPDATE users SET ";
 	$query .= "user_firstname = '{$user_firstname}', ";
 	$query .= "user_lastname = '{$user_lastname}', ";
 	$query .= "user_role = '{$user_role}', ";
 	$query .= "username = '{$username}', ";
 	$query .= "user_email = '{$user_email}', ";
-	$query .= "user_password = '{$user_password}' ";
+	$query .= "user_password = '{$hashed_password}' ";
 	$query .= "WHERE user_id = {$the_user_id} ";
 
 	$edit_user_query = mysqli_query($connection, $query);
 
 	confirmQuery($edit_user_query);
+
+	echo "<p class='bg-success'>User Updated. <a class='text-danger' href='../admin/users.php'>View All Users</a></p>";
 
 }
 
@@ -66,7 +80,7 @@ if (isset($_POST['edit_user'])) {
 	<div class="form-group">
 		<select name="user_role" id="">
 			
-			<option value="subscriber"><?php echo $user_role; ?></option>
+			<option value="<?php echo $user_role; ?>"><?php echo $user_role; ?></option>
 			<?php 
 
 			if ($user_role == 'admin') {
