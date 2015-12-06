46f3bbce735a0373d227dc950eb098f19e68ed1f
Shair Mir <s.mir89@hotmail.com>
2015-12-06 12:37:59 +0100

    Finish crypting passwords

--- registration.php
+++ registration.php
@@ -23,6 +23,7 @@ if(isset($_POST['submit'])) {
 
         $row = mysqli_fetch_array($select_randsalt_query);
         $salt = $row['randSalt'];
+        $password = crypt($password, $salt);
 
         $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
         $query .= "VALUES('{$username}', '{$email}', '{$password}', 'subscriber' ) ";
