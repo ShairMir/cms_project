6434139a6d6ee87b0b7c1f91da7e510ff6301180
Shair Mir <s.mir89@hotmail.com>
2015-12-08 20:11:41 +0100

    Added font icons and removed bloat

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -146,8 +146,8 @@ if (isset($_POST['checkBoxArray'])) {
                     echo "<td>{$post_comment_count}</td>";
                     echo "<td>{$post_date}</td>";
                     echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to reset the view count?')\" href='posts.php?reset=$post_id' >$post_views_count</a></td>";
-                    echo "<td class='text-center'><a href='posts.php?source=edit_post&p_id={$post_id}'><i class='fa fa-pencil-square-o fa-lg'></i></a></td>";
-                    echo "<td class='text-center'><a onClick=\"javascript: return confirm('Are you sure you want to delete this post?')\" href='posts.php?delete=$post_id'><i class='fa fa-times fa-lg'></i></a></td>";
+                    echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'><i class='fa fa-pencil-square-o fa-lg'></i></a></td>";
+                    echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this post?')\" href='posts.php?delete=$post_id'><i class='fa fa-times fa-lg'></i></a></td>";
                     echo "</tr>";
                 }        
             } 
--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -7,6 +7,10 @@
             <th>Lastname</th>
             <th>Email</th>
             <th>Role</th>
+            <th>Admin</th>
+            <th>Subscriber</th>
+            <th>Edit</th>
+            <th>Delete</th>
         </tr>
     </thead>
     <tbody>
@@ -48,8 +52,8 @@
 
                 echo "<td><a href='users.php?change_to_admin=$user_id''>Admin</a></td>";
                 echo "<td><a href='users.php?change_to_subscriber=$user_id''>Subscriber</a></td>";
-                echo "<td><a href='users.php?source=edit_user&edit_user=$user_id''>Edit</a></td>";
-                echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this user?')\" href='users.php?delete=$user_id'>Delete</a></td>";
+                echo "<td><a href='users.php?source=edit_user&edit_user=$user_id''><i class='fa fa-pencil-square-o fa-lg'></i></a></td>";
+                echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this user?')\" href='users.php?delete=$user_id'><i class='fa fa-times fa-lg'></i></a></td>";
                 echo "</tr>";
             }        
         } 
