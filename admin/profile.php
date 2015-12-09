179d03b271f2595d04a65fc2275cc5be32a248a2
Shair Mir <s.mir89@hotmail.com>
2015-12-09 15:17:59 +0100

    fixed displaying user role

--- admin/profile.php
+++ admin/profile.php
@@ -109,7 +109,7 @@ if (isset($_POST['edit_user'])) {
                             <div class="form-group">
                                 <select name="user_role" id="">
                                     
-                                    <option value="subscriber"><?php echo $user_role; ?></option>
+                                    <option value="<?php echo $user_role; ?>"><?php echo $user_role; ?></option>
                                     <?php 
 
                                     if ($user_role == 'admin') {
