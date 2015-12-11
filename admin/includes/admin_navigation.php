64764915d54a15f1948dbfe6e18f46452faee506
Shair Mir <s.mir89@hotmail.com>
2015-12-11 20:03:19 +0100

    Display users online

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -12,6 +12,9 @@
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
+                
+                <li><a href="">Users Online: <?php echo users_online() ?></a></li>
+
                 <li><a href="../index.php">Home Site</a></li>
                 
                
