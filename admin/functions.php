03a08fae2997434f7523293af19088591783ce93
Shair Mir <s.mir89@hotmail.com>
2015-12-11 20:22:16 +0100

    Instant users online count

--- admin/functions.php
+++ admin/functions.php
@@ -2,29 +2,36 @@
 
 function users_online() {
 
-    global $connection;
+    if(isset($_GET['onlineusers'])) {
 
-    $session = session_id();
-    $time = time();
-    $time_out_in_seconds = 30;
-    $time_out = $time - $time_out_in_seconds;
+        global $connection;
 
-    $query = "SELECT * FROM users_online WHERE session = '$session'";
-    $send_query = mysqli_query($connection, $query);
-    $count = mysqli_num_rows($send_query);
+        if (!$connection) {
+            session_start();
+            include("../includes/db.php");
 
-    if ($count == NULL) {
-        mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
-    } else {
-        mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
-    }
+            $session = session_id();
+            $time = time();
+            $time_out_in_seconds = 60;
+            $time_out = $time - $time_out_in_seconds;
 
-    $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out' ");
-    return $count_user = mysqli_num_rows($users_online_query);
+            $query = "SELECT * FROM users_online WHERE session = '$session'";
+            $send_query = mysqli_query($connection, $query);
+            $count = mysqli_num_rows($send_query);
 
+            if ($count == NULL) {
+                mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+            } else {
+                mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+            }
+
+            $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out' ");
+            echo $count_user = mysqli_num_rows($users_online_query);
+        }       
+    } // get request
 }
 
-
+users_online();
 
 
 function confirmQuery($result) {
--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -13,7 +13,9 @@
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
                 
-                <li><a href="">Users Online: <?php echo users_online() ?></a></li>
+                <!-- <li><a href="">Users Online: <?php //echo users_online() ?></a></li> -->
+
+                <li><a href="">Users Online: <span class="usersonline"></span></a></li>
 
                 <li><a href="../index.php">Home Site</a></li>
                 
