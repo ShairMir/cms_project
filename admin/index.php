a0a42b0bb4192b14d0cf625a05d696be7c7eb1b5
Shair Mir <s.mir89@hotmail.com>
2015-12-11 19:58:46 +0100

    added users_online function

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,7 @@
 <?php include "includes/admin_header.php"; ?>
 
 <div id="wrapper">
+
     <!-- Navigation -->
     <?php include "includes/admin_navigation.php"; ?>
 
@@ -12,12 +13,10 @@
             <div class="row">
                 <div class="col-lg-12">
 
-
                     <h1 class="page-header">
                         Welcome to the Dashboard
                         <small><?php echo $_SESSION['username']; ?></small>
                     </h1>
-
 
                 </div>
             </div>
