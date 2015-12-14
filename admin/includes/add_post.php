7a1261db8857705d441733c83471f854465a3d4f
Shair Mir <s.mir89@hotmail.com>
2015-12-14 11:50:40 +0100

    Displaying Post User
    - Changed all post_author to post_user
    - Added dropdown for selecting users

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -3,7 +3,7 @@
 if (isset($_POST['create_post'])) {
 	
 	$post_title = $_POST['title'];
-	$post_author = $_POST['author'];
+	$post_user = $_POST['post_user'];
 	$post_category_id = $_POST['post_category'];
 	$post_status = $_POST['post_status'];
 
@@ -16,8 +16,8 @@ if (isset($_POST['create_post'])) {
 
 	move_uploaded_file($post_image_temp, "../images/$post_image");
 
-	$query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
-	$query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}' ) ";
+	$query = "INSERT INTO posts(post_category_id, post_title, post_user, post_date, post_image, post_content, post_tags, post_status) ";
+	$query .= "VALUES({$post_category_id},'{$post_title}','{$post_user}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}' ) ";
 
 	$create_post_query = mysqli_query($connection, $query);
 
@@ -43,6 +43,7 @@ if (isset($_POST['create_post'])) {
 	</div>
 	
 	<div class="form-group">
+		<label for="title">Category</label>
 		<select name="post_category" id="">
 			
 			<?php 
@@ -67,13 +68,40 @@ if (isset($_POST['create_post'])) {
 	</div>
 	
 	<div class="form-group">
+		<label for="title">Users</label>
+		<select name="post_user" id="">
+			
+			<?php 
+
+			$user_query = "SELECT * FROM users";
+			$select_users = mysqli_query($connection, $user_query);
+
+			confirmQuery($select_users);
+
+			while ($row = mysqli_fetch_assoc($select_users)) {
+				$user_id = $row['user_id'];
+				$username = $row['username'];
+
+				echo "<option value='$username'>{$username}</option>";
+			}
+
+			?>
+
+		</select>
+	</div>
+
+
+
+
+<!-- 	<div class="form-group">
 		<label for="title">Post Author</label>
 		<input type="text" class="form-control" name="author">
-	</div>
+	</div> -->
 
 
 	
 	<div class="form-group">
+
 		<select name="post_status" id="">
 			<option value="draft">Post Status</option>
 			<option value="published">Publish</option>
