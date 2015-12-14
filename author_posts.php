c68204ca348172bffc6070de532c8361b27ac999
Shair Mir <s.mir89@hotmail.com>
2015-12-14 13:15:11 +0100

    Show all posts by user

--- author_posts.php
+++ author_posts.php
@@ -11,26 +11,22 @@
             <!-- Blog Entries Column -->
 
             <div class="col-md-8">
-                
-               
-
-               
     
                 <?php 
 
                 if (isset($_GET['p_id'])) {
                     $the_post_id = $_GET['p_id'];
-                    $the_post_author = $_GET['author'];
+                    $the_post_user = $_GET['user'];
 
-                    echo "<h1 class='page-header'>All posts by $the_post_author</h1>";      
+                    echo "<h1 class='page-header'>All posts by $the_post_user</h1>";      
                 }
 
-                $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}' ";
+                $query = "SELECT * FROM posts WHERE post_user = '{$the_post_user}' ORDER BY post_id DESC ";
                 $select_all_posts_query = mysqli_query($connection, $query);
 
                 while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                     $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
+                    $post_user = $row['post_user'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
                     $post_content = $row['post_content'];
@@ -40,11 +36,11 @@
 
                     <!-- Blog Post -->
                     <h2>
-                        <a href="#"><?php echo $post_title ?></a>
+                        <a href="#>"><?php echo $post_title ?></a>
                     </h2>
-                    <p class="lead">
-                        by <?php echo $post_author ?>
-                    </p>
+                    <!-- <p class="lead">
+                        by <?php //echo $post_user ?>
+                    </p> -->
                     <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                     <hr>
                     <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
