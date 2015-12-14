dcf84fe926e1328c177abeaab1889f0e76a09512
Shair Mir <s.mir89@hotmail.com>
2015-12-14 11:49:31 +0100

    Displaying post user instead of author
    - changed all post_author to post_user
    - Showing current user on dropdown

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -11,7 +11,7 @@ $select_posts_by_id = mysqli_query($connection, $query);
 
 while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_id = $row['post_id'];
-    $post_author = $row['post_author'];
+    $post_user = $row['post_user'];
     $post_title = $row['post_title'];
     $post_category_id = $row['post_category_id'];
     $post_status = $row['post_status'];
@@ -22,7 +22,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_date = $row['post_date'];
 
     $post_id = mysqli_real_escape_string($connection, $post_id);
-    $post_author = mysqli_real_escape_string($connection, $post_author);
+    $post_user = mysqli_real_escape_string($connection, $post_user);
     $post_title = mysqli_real_escape_string($connection, $post_title);
     $post_category_id = mysqli_real_escape_string($connection, $post_category_id);
     $post_status = mysqli_real_escape_string($connection, $post_status);
@@ -36,7 +36,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
 if (isset($_POST['update_post'])) {
 
-    $post_author = $_POST['post_author'];
+    $post_user = $_POST['post_user'];
     $post_title = $_POST['post_title'];
     $post_category_id = $_POST['post_category'];
     $post_status = $_POST['post_status'];
@@ -60,7 +60,7 @@ if (isset($_POST['update_post'])) {
     $query .= "post_title = '{$post_title}', ";
     $query .= "post_category_id = '{$post_category_id}', ";
     $query .= "post_date = now(), ";
-    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_user = '{$post_user}', ";
     $query .= "post_status = '{$post_status}', ";
     $query .= "post_tags = '{$post_tags}', ";
     $query .= "post_content = '{$post_content}', ";
@@ -108,11 +108,33 @@ if (isset($_POST['update_post'])) {
 		</select>
 	</div>
 
-	
 	<div class="form-group">
-		<label for="title">Post Author</label>
-		<input value="<?php echo $post_author;?>" type="text" class="form-control" name="post_author">
+		<label for="title">Users</label>
+		<select name="post_user" id="">
+			<?php echo "<option value='$post_user'>{$post_user}</option>"; ?>
+			<?php 
+
+			$user_query = "SELECT * FROM users";
+			$select_users = mysqli_query($connection, $user_query);
+
+			confirmQuery($select_users);
+
+			while ($row = mysqli_fetch_assoc($select_users)) {
+				$user_id = $row['user_id'];
+				$username = $row['username'];
+
+				echo "<option value='$username'>{$username}</option>";
+			}
+
+			?>
+
+		</select>
 	</div>
+	
+	<!-- <div class="form-group">
+		<label for="title">Post Author</label>
+		<input value="<?php //echo $post_user;?>" type="text" class="form-control" name="post_user">
+	</div> -->
 
 	<div class="form-group">
 		<label for="title">Post Status</label><br>
