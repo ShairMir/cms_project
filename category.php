c6736c9cfaab5a3031bd61e33b90da2815434c3c
Shair Mir <s.mir89@hotmail.com>
2015-12-18 22:39:24 +0100

    fixed author name

--- category.php
+++ category.php
@@ -37,7 +37,7 @@
                         while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                             $post_id = $row['post_id'];
                             $post_title = $row['post_title'];
-                            $post_author = $row['post_author'];
+                            $post_user = $row['post_user'];
                             $post_date = $row['post_date'];
                             $post_image = $row['post_image'];
                             $post_content = substr($row['post_content'], 0, 150) . "...";
@@ -50,7 +50,7 @@
                                 <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                             </h2>
                             <p class="lead">
-                                by <a href="index.php"><?php echo $post_author ?></a>
+                                by <a href="index.php"><?php echo $post_user ?></a>
                             </p>
                             <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                             <hr>
