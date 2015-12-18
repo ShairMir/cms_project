56bd72696650412de4d30bb3d80facd83e5510b2
Shair Mir <s.mir89@hotmail.com>
2015-12-18 13:41:03 +0100

    New contact form

--- contact.php
+++ contact.php
@@ -4,31 +4,13 @@
 
 if(isset($_POST['submit'])) {
 
-    $username = escape($_POST['username']);
-    $email    = escape($_POST['email']);
-    $password = escape($_POST['password']);
+    $to         = "s.mir89@hotmail.com";
+    $subject    = $_POST['subject'];
+    $body       = wordwrap($_POST['body'], 70);
+    $header     = "From: " . $_POST['email'];
 
-    if(!empty($username) && !empty($email) && !empty($password)) {
+    mail($to, $subject, $body, $header);
 
-        $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12) );
-
-        $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
-        $query .= "VALUES('{$username}', '{$email}', '{$password}', 'subscriber' ) ";
-        $register_user_query = mysqli_query($connection, $query);
-
-        if (!$register_user_query) {
-            die("QUERY FAILED" . mysqli_error($connection) . ' ' . mysqli_errno($connection) );
-        }
-
-        $message = "Your Registration has been submitted ";
-
-    } else {
-
-        $message = "Fields can not be empty";
-
-    }
-} else {
-    $message = "";
 }
 
 ?>
@@ -42,28 +24,27 @@ if(isset($_POST['submit'])) {
     <!-- Page Content -->
     <div class="container">
     
-    <section id="login">
+    <section id="contact">
         <div class="container">
             <div class="row">
                 <div class="col-xs-6 col-xs-offset-3">
                     <div class="form-wrap">
                     <h1>Contact</h1>
-                        <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
-                            <h6 class="text-center"><?php echo $message ?></h6>
-                            <div class="form-group">
-                                <label for="username" class="sr-only">username</label>
-                                <input type="text" name="username" id="username" class="form-control" placeholder="New Username">
-                            </div>
+                        <form role="form" action="contact.php" method="post" id="login-form" autocomplete="off">
+                            
                              <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
-                                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email">
+                            </div>
+                            <div class="form-group">
+                                <label for="subject" class="sr-only">Subject</label>
+                                <input type="text" name="subject" id="subject" class="form-control" placeholder="Enter your subject">
                             </div>
                              <div class="form-group">
-                                <label for="password" class="sr-only">Password</label>
-                                <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                                <textarea class="form-control" name="body" id="body" cols="50" rows="10"></textarea>
                             </div>
                     
-                            <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                            <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Submit">
                         </form>
                      
                     </div>
