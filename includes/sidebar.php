e1ac3917976f70ab4ef4fc7522101cc087d1c8f9
Shair Mir <s.mir89@hotmail.com>
2015-12-18 23:43:01 +0100

    New sidebar for logged in users

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -19,19 +19,30 @@
 
                 <!-- Login -->
                 <div class="well">
-                    <h4>Login</h4>
-                    <form action="includes/login.php" method="post">
-                        <div class="form-group">
-                            <input name="username" type="text" class="form-control" placeholder="Username">
-                        </div>
-                        <div class="input-group">
-                            <input name="password" type="password" class="form-control" placeholder="Password">
-                            <span class="input-group-btn">
-                                <button class="btn btn-primary" name="login" type="submit">Submit</button>
-                            </span>
-                        </div>
-                    </form> <!-- login form -->
-                    <!-- /.form-group -->
+
+                    <?php if(isset($_SESSION['user_role'])): ?>
+                        
+                        <h4>Logged in as <?php echo $_SESSION['username']; ?></h4>
+                        <a href="includes/logout.php" class="btn btn-primary">Logout</a>
+
+                    <?php else: ?>
+
+                        <h4>Login</h4>
+                        <form action="includes/login.php" method="post">
+                            <div class="form-group">
+                                <input name="username" type="text" class="form-control" placeholder="Username">
+                            </div>
+                            <div class="input-group">
+                                <input name="password" type="password" class="form-control" placeholder="Password">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-primary" name="login" type="submit">Submit</button>
+                                </span>
+                            </div>
+                        </form> <!-- login form -->
+                        <!-- /.form-group -->
+
+                    <?php endif; ?>
+
                 </div>
 
                 <!-- Blog Categories Well -->
