0aba3ef84c74442034993a08bf87117d11b17741
Shair Mir <s.mir89@hotmail.com>
2015-12-18 23:43:37 +0100

    fixed current displayed categories

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -90,7 +90,12 @@ if ($_SESSION['user_role'] == 'admin') {
 				$cat_id = $row['cat_id'];
 				$cat_title = $row['cat_title'];
 
-				echo "<option value='$cat_id'>{$cat_title}</option>";
+				if ($cat_id == $post_category_id) {
+					echo "<option selected value='$cat_id'>{$cat_title}</option>";
+				} else {
+					echo "<option value='$cat_id'>{$cat_title}</option>";	
+				}
+				
 
 			}
 
