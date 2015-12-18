359412cd285c555c86f15ca66dd20762304bdd23
Shair Mir <s.mir89@hotmail.com>
2015-12-18 21:24:04 +0100

    show category name in title

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,8 +23,8 @@
                 while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
                         $cat_title = $row['cat_title'];
                         $cat_id = $row['cat_id'];
-                        
-                        echo "<li> <a href='category.php?category=$cat_id'>{$cat_title}</a> </li>";
+
+                        echo "<li> <a href='category.php?category=$cat_id&title=$cat_title'>{$cat_title}</a> </li>";
                     }    
                 ?>      
 
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -52,10 +52,10 @@
                                 <?php
 
                                 while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
-                                $cat_title  = $row['cat_title'];
-                                $cat_id     = $row['cat_id'];
+                                    $cat_title  = $row['cat_title'];
+                                    $cat_id     = $row['cat_id'];
 
-                                echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
+                                    echo "<li><a href='category.php?category=$cat_id&title=$cat_title'>{$cat_title}</a></li>";
                                 } 
                                 
                                 ?>
