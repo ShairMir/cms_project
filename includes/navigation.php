48fbfb742773099996e9ac522939cb63277cca01
Shair Mir <s.mir89@hotmail.com>
2015-12-18 16:32:41 +0100

    show all category posts

--- includes/navigation.php
+++ includes/navigation.php
@@ -22,7 +22,9 @@
 
                 while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
                         $cat_title = $row['cat_title'];
-                        echo "<li> <a href='#'>{$cat_title}</a> </li>";
+                        $cat_id = $row['cat_id'];
+                        
+                        echo "<li> <a href='category.php?category=$cat_id'>{$cat_title}</a> </li>";
                     }    
                 ?>      
 
